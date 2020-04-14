beece45c9916dc3499be4f15f86bb7517bc93ebd
Lu <julu@nrn.nrcan.gc.ca>
2020-04-14 15:00:51 -0400

    Added info windows for graphing page

--- webform-expressapp/views/mapiframe/selectplugin.js
+++ webform-expressapp/views/mapiframe/selectplugin.js
@@ -6,6 +6,8 @@ window.select_coordinates = {
     showRecords: true,
     click: null,
     api: null,
+    bundle: null,
+    data: null,
     init(rampApi) {
         this.api = rampApi; 
         this.listenToMapAdd(); 
@@ -19,7 +21,16 @@ window.select_coordinates = {
     },
     listenToMapAdd() {
         RAMP.mapAdded.subscribe(() => {
-            this.listenToClick(); 
+            const esriApi = RAMP.GAPI.esriLoadApiClasses([
+                ["esri/InfoTemplate", "InfoTemplate"], 
+                ["esri/dijit/InfoWindow", "InfoWindow"]
+            ]); 
+
+            esriApi.then((bundle) => {
+                this.bundle = bundle; 
+                this.listenToClick(); 
+            })
+
         }); 
     },
     listenToClick() {
@@ -43,6 +54,7 @@ window.select_coordinates = {
             else if (e.data == "toggle record") {
                 this.showRecords = !this.showRecords; 
                 recordLayer.esriLayer.setVisibility(this.showRecords); 
+                this.api.esriMap.infoWindow.hide(); 
             }
             else {
                 return; 
@@ -87,6 +99,7 @@ window.select_coordinates = {
             markerLayer.addGeometry(rectangle);
             
             $.getJSON("http://localhost:8080/api?boundingbox=" + boundingcoords, (data) => {
+                this.data = data; 
                 var idcounter = 100;
                 for (const record of data) {
                     var stringlist = record.fullbox.split("(").join("").split(")").join("").split(",");
@@ -95,13 +108,55 @@ window.select_coordinates = {
                     for (var i = 0; i < stringlist.length; i+= 2) {
                         boxlist.push([parseFloat(stringlist[i + 1]), parseFloat(stringlist[i])]); 
                     } 
-                    var recordbox = new RAMP.GEO.Polygon(idcounter, boxlist, { outlineColor: [255, 130, 0], 
+                    var recordbox = new RAMP.GEO.Polygon(record.submissionid, boxlist, { outlineColor: [255, 130, 0], 
                                                                             fillColor: [255, 130, 0], fillOpacity:0.6,
                                                                             outlineWidth: 3 }); 
                     recordLayer.addGeometry(recordbox); 
                     idcounter++; 
                 }
             }).then(() => {
+                //prepare infowindows for each record
+                for (const record of recordLayer.esriLayer.graphics) {
+                    const recordTemplate = new this.bundle.InfoTemplate();
+                    const recordData = this.data.filter((rec) => rec.submissionid === parseInt(record.geometry.apiId))[0]; 
+                    recordTemplate.setTitle(record.apiId); 
+                    recordTemplate.setContent(`Age of Mapping: ${recordData.lastprojupdate}</br>
+                                                Flood Hazard Standard: ${recordData.floodhzdstd}</br>
+                                                Project ID: ${recordData.projectid}</br>
+                                                Project Name: ${recordData.projectname}</br>
+                                                Official WC Name: ${recordData.officialwcname}</br>
+                                                Local WC Name: ${recordData.localwcname}</br>
+                                                <button type="button" id="${recordData.submissionid}" onClick=downloadCSV(this.id)>Download CSV of Records</button>`);
+                    record.setInfoTemplate(recordTemplate); 
+                }
+
+                downloadCSV = (id) => {
+                    var csvContent = "data:text/csv;charset=utf-8,"; 
+                    var data = this.data.filter((rec) => rec.submissionid === parseInt(id));
+                    if (data.length > 0) {
+                        //write the fields first
+                        var fieldsRow = Object.keys(data[0]).join(","); 
+                        csvContent += fieldsRow + "\r\n";
+                        for (const record of data){
+                            for (const entry of Object.values(record)) {
+                                let content = entry; 
+                                if (typeof(content) === "string" && content.includes(",")) {
+                                    content = "\"" + content + "\""; 
+                                }
+                                csvContent += content + ","
+                            }
+                            //let contentRow = Object.values(record).join(","); 
+                            csvContent += "\r\n";
+                        }
+                        var encodedUri = encodeURI(csvContent); 
+                        var download = document.createElement("a"); 
+                        download.href = encodedUri; 
+                        download.target = "_blank"; 
+                        download.download = `${id}.csv`; 
+                        download.click(); 
+                    }
+                }
+
                 //post a message
                 //disable the event handler
                 window.parent.postMessage("coordinates selected " + boundingcoords, "*");
@@ -113,6 +168,7 @@ window.select_coordinates = {
         //remove all geometry OR pass in an array of String? geometry IDs as param....  
         markerLayer.removeGeometry(); 
         recordLayer.removeGeometry(); 
+        this.api.esriMap.infoWindow.hide(); 
         this.coordslist = []; 
         this.reqcoordslist = []; 
         this.countClicks = 0;
